ba60cad9002b712266a43a49a4895514021990d2
ludi2310 <67338401+ludi2310@users.noreply.github.com>
2020-07-10 11:32:34 +0200

    Update bargain_shop.asm

--- data/items/bargain_shop.asm
+++ data/items/bargain_shop.asm
@@ -1,10 +1,10 @@
 BargainShopData:
 	db 7
-	dbw NUGGET,       4500
+	dbw NUGGET,        100
 	dbw BIG_NUGGET,    100
 	dbw PEARL,         650
-	dbw BIG_PEARL,    3500
-	dbw PEARL_STRING, 6500
-	dbw STARDUST,     1000
-	dbw STAR_PIECE,   4600
+	dbw BIG_PEARL,     100
+	dbw PEARL_STRING,  100
+	dbw STARDUST,      100
+	dbw STAR_PIECE,    100
 	db -1
